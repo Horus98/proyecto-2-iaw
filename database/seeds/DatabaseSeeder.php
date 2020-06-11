bbf7e7963e32c35bfc5d3461ffe71851a0203521
Horus <horusmoro2015@gmail.com>
2020-06-11 10:26:35 -0300

    Revert "Revert "Update""
    This reverts commit 4c2fcc2988ed91cddc673d793248324ab80c17f2.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(CarSeeder::class);
+        $this->call(SalesSeedeer::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
+Route::get('/Ruta1', function () {
+    return view('ruta1');
+})->name('ruta1');
+
+Route::get('/Stock','CarController@index', function () {
+    return view('Stock');
+})->name('Stock');
