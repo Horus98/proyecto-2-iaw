49efafba59e0a5c1be8a55888de1818feb7e1314
Horus <horusmoro2015@gmail.com>
2020-06-30 18:07:23 -0300

    DataTable upgrade.
    Se agrega la funcionalidad de dataTables a la ventana Stock del empleado.

--- resources/views/consultaBD.blade.php
+++ resources/views/consultaBD.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
 
 <style type="text/css">.img{width:200px;height:120px;} </style>
 <nav class="navbar navbar-light bg-light">
@@ -60,11 +61,12 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"> </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    @include('partials.Scripts')
    
     <script>
+    $(document).ready(function() {
+    $('#example').DataTable();
+    } );
         window.setTimeout(function() {
             $("#alert").fadeTo(500, 0).slideUp(500, function(){
             $(this).remove(); 
